52e34b588cf54661dccf0f653b6a4090457654e0
Nachalo Letsunyane <nachalo@itiq.co.bw>
2025-06-22 21:09:49 +0200

    Revoked and decoupled secrets and keys

--- webui/src/config/firebase.ts
+++ webui/src/config/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics'
 const firebaseConfig = {
   // These will be populated from your Firebase project
   // For now, using placeholder values - you'll need to replace these
-  apiKey: "AIzaSyC0qsS6umf734-hGLMhvXM3isH6Y3rbxak",
-  authDomain: "quickrest-8d903.firebaseapp.com",
-  projectId: "quickrest-8d903",
-  storageBucket: "quickrest-8d903.firebasestorage.app",
-  messagingSenderId: "40119968782",
-  appId: "1:40119968782:web:14c7b602a93fbdc946485f",
-  measurementId: "G-Y3RXWPNT1W"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
